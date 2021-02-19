dd236981891fb6891ee7cda7d40caacc793f6801
vladilyushchenko <vladislavilyushchenko@gmail.com>
2021-02-19 10:46:59 +0300

    19.02 10:46 commit

--- src/main/java/MatrixParallel.java
+++ src/main/java/MatrixParallel.java
@@ -2,11 +2,6 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class MatrixParallel {
 
-    public static void main(String[] args){
-        int a = 5;
-        int b = 100;
-    }
-
     public static double maxRowSum(int threadNum, double[][] matrix, int columns) throws Exception {
         double result;
         if (threadNum < 1) throw new Exception("Incorrect number of threads!");
@@ -14,32 +9,23 @@ public class MatrixParallel {
         MatrixThread.resetSum();
 
         if (threadNum < matrix.length) {
-            int sliceSize = (int) Math.round(matrix.length / (double) threadNum);
+            int sliceSize = matrix.length / threadNum;
 
-            int i = 0, rowsLeft = matrix.length;
-            if (sliceSize == 1) {
-                for (i = 0; i < matrix.length - threadNum; i++) {
-                    threads[i] = new MatrixThread((i == 0) ? 0 : (i - 1) * 2 + 2, 2, matrix, columns);
+            int i = 0;
+            if (sliceSize * threadNum < matrix.length) {
+                int currSlice =  sliceSize + 1;
+                for (i = 0; i < matrix.length % threadNum; i++) {
+                    threads[i] = new MatrixThread(i * currSlice, currSlice, matrix, columns);
                     threads[i].start();
-                    rowsLeft -= 2;
                 }
             } else {
                 threads[i] = new MatrixThread(0, sliceSize, matrix, columns);
                 threads[i++].start();
-                rowsLeft -= sliceSize;
             }
 
             for (; i < threadNum; i++) {
-                if (rowsLeft < sliceSize) {
-                    threads[i] = new MatrixThread(
-                            threads[i - 1].getFirstPosition() + threads[i - 1].getSliceSize(),
-                            rowsLeft, matrix, columns);
-                } else {
-                    threads[i] = new MatrixThread(
-                            threads[i - 1].getFirstPosition() + threads[i - 1].getSliceSize(),
-                            sliceSize, matrix, columns);
-                    rowsLeft -= sliceSize;
-                }
+                threads[i] = new MatrixThread(threads[i - 1].getFirstRow() + threads[i - 1].getSliceSize(),
+                        sliceSize, matrix, columns);
                 threads[i].start();
             }
         } else {
@@ -60,22 +46,20 @@ public class MatrixParallel {
 
     private static class MatrixThread extends Thread {
         private static double sum = 0.0;
-        private final int firstPosition, sliceSize, columns;
+        private final int firstRow, sliceSize, columns;
         private final double[][] matrix;
-        private static final  ReentrantLock locker = new ReentrantLock();
+        private static final ReentrantLock locker = new ReentrantLock();
 
         public static void resetSum() {
             sum = 0.0;
         }
 
-        public double getSum() {
-            return sum;
-        }
-        public int getFirstPosition() { return firstPosition; }
+        public double getSum() { return sum; }
+        public int getFirstRow() { return firstRow; }
         public int getSliceSize() { return sliceSize; }
 
-        public MatrixThread(int firstPosition, int sliceSize, double[][] matrix, int columns) {
-            this.firstPosition = firstPosition;
+        public MatrixThread(int firstRow, int sliceSize, double[][] matrix, int columns) {
+            this.firstRow = firstRow;
             this.sliceSize = sliceSize;
             this.matrix = matrix;
             this.columns = columns;
@@ -83,7 +67,7 @@ public class MatrixParallel {
 
         @Override
         public void run() {
-            for (int i = firstPosition; i < firstPosition + sliceSize; i++) {
+            for (int i = firstRow; i < firstRow + sliceSize; i++) {
                 double rowMax = Double.MIN_VALUE;
                 for (int j = 0; j < columns; j++) {
                     if (matrix[i][j] > rowMax) {
